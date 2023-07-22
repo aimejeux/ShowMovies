821763f03ede5f0679e80e5a5c59b1a5571c40ae
AbouYacine [aime_jeux] <aime_jeux@yahoo.fr>
2023-07-22 22:39:42 +0100

    Update MyImportCimalek.py

--- Cimalek/OutilsCimalek/MyImportCimalek.py
+++ Cimalek/OutilsCimalek/MyImportCimalek.py
@@ -79,26 +79,33 @@ def export_txt(txt):
         f.write(txt)
     f.close()
     return 'Mission accomplished'
+####################################################################### Lire Js File
 def Read_Js():
     with open(cfg_path) as mon_Js:
         data = json.load(mon_Js)
-    #print type(data)
     return data
-#######################################################################Get Data From Js File
-def get_Data(url,get='',typ='',param=None,headers=None):
+#######################################################################Get Data From Js or Content File
+def get_Data(url,get='',typ='',param=False,headers=False):
+    print 'url = ',url
+    print 'get = ',get
+    print 'typ = ',typ
+    print 'param = ',param
+    print 'headers = ',headers
+    print 'type(headers) = ',type(headers)
     _Data = ''
     if param and headers:
-        try:exec "_Data = St."+get+"(url,data=param,heades=headers,verify=False,timeout=10)."+typ
+        try:exec "_Data = St."+get+"(url,data=param,headers=headers,verify=False)."+typ
         except:_Data = ''
     elif param and not headers:
         try:exec "_Data = St."+get+"(url,data=param,verify=False,timeout=10)."+typ
         except:_Data = ''
     elif headers and not param :
-        try:exec "_Data = St."+get+"(url,heades=headers,verify=False,timeout=10)."+typ
+        try:
+            exec ("_Data = St."+get+"(url,headers=headers,verify=False,timeout=10)."+typ)
         except:_Data = ''
     else :
-        try: exec "_Data=St."+get+"(url,verify=False,timeout=10)."+typ
-        except:_Data = '' 
+        try: exec ("_Data=St."+get+"(url,verify=False,timeout=10)."+typ)
+        except:_Data = ''
     return _Data
 #######################################################################Import Site Data
 def get_My_Donnees(url):
@@ -111,16 +118,15 @@ def get_My_Donnees(url):
         pagination = ClearTitle(pagination)
         pagination = pagination.split()
     except:pagination='nada'
+    pagination
     blocks=Donnees.split('<div class="film-poster"')
     blocks.pop(0)
-    i = 1
     for block in blocks:
         try:
             rgx = '''data-id="(.+?)"'''
             Id = re.findall(rgx,block,re.M|re.I|re.DOTALL)[0]
         except:
             Id= 'nada'
-        #print Id
         try:
             rgx = '''<a href="(.+?)">.+?<img'''
             Href = re.findall(rgx,block,re.M|re.I|re.DOTALL)[0]
@@ -134,19 +140,16 @@ def get_My_Donnees(url):
             Downloads_Images(img,dist)
         except:
             img= 'nada'
-        #print img
         try:
             rgx = '''<div class="rating">(.+?)</div>'''
             rating = re.findall(rgx,block,re.M|re.I|re.DOTALL)[0]
         except:
             rating= 'nada'
-        #print rating
         try:
             rgx = '''<div class="quality">(.+?)</div>'''
             quality = re.findall(rgx,block,re.M|re.I|re.DOTALL)[0]
         except:
             quality= 'nada'
-        #print quality
         try:
             rgx = '''<div class="title">(.+?)</div>'''
             title = re.findall(rgx,block,re.M|re.I|re.DOTALL)[0].decode('utf-8')
@@ -154,15 +157,129 @@ def get_My_Donnees(url):
             title = ClearTitle(title)
         except:
             title= 'title'
-        #print title
         try:
             rgx = '''<div class="desc">(.+?)</div>'''
             desc = re.findall(rgx,block,re.M|re.I|re.DOTALL)[0].decode('utf-8')
         except:
             desc= 'nada'
-        #print desc
         MyDict_[Id]=[title,Href,img,nam_img,rating,quality,desc]
-        #print "==========================================",str(i)
-        i = i + 1
     TXT = json.dumps(MyDict_)
     return True,export_txt(TXT),pagination
+#######################################################################Import Info_Film
+def get_Info_Film(url):
+    MyDict_ = {}
+    Discrpt_3 = []
+    print url
+    Donnees = get_Data(url,get='get',typ='content')
+    if Donnees == '':return False,Donnees
+    tmx = '''<div class="anisc-detail">(.+?)<div class="module_single_sda">'''
+    try:
+        blocks = re.findall(tmx,Donnees,re.M|re.I|re.DOTALL)[0].decode('utf-8')
+    except:blocks='nada'
+    if blocks!='nada':
+        try:
+            rgx = '''<a href="(.+?)" class="watchBTn">'''
+            watchBTn = re.findall(rgx,blocks,re.M|re.I|re.DOTALL)[0]
+        except:watchBTn='nada'
+        MyDict_['watchBTn']=watchBTn
+        try:
+            rgx = '''<div class="text">(.+?)</div>'''
+            Discrpt = re.findall(rgx,blocks,re.M|re.I|re.DOTALL)[0]
+        except:Discrpt='nada'
+        MyDict_['Discrpt']=Discrpt
+        try:
+            rgx = '''<span class="item-head">'''
+            Discrpt_1 = blocks.split(rgx)
+            Discrpt_1.pop(0)
+        except:Discrpt_1='nada'
+        try:
+            gbx = '''<div class="item">.+?<span>(.+?)</span>.+?</div>'''
+            Discrpt_2 = re.findall(gbx,blocks,re.M|re.I|re.DOTALL)
+        except:Discrpt_2='nada'
+        if Discrpt_2!='nada':
+            for f in Discrpt_2:
+                if 'href' in f:continue
+                Discrpt_3.append((f))
+                print f
+            MyDict_['Discrpt_2']=Discrpt_3
+        try:
+            gbx = '''<span class="text"><span>(.+?)</span> من <span>(.+?)</span></span>'''
+            Rat = re.findall(gbx,Donnees,re.M|re.I|re.DOTALL)
+            Rating = Rat[0][0]+'/'+Rat[0][1]
+        except:Rating='nada'
+        MyDict_['Rating']=Rating
+        try:
+            gnx = '''<div class="item-list">(.+?)<div class="film-description w-hide">'''
+            genres = re.findall(gnx,Donnees,re.S|re.M|re.I|re.DOTALL)[0]
+            genres = genres.replace('\n','').replace('\t','').replace('\r','').replace('\s','')
+            genre = re.findall('>(.+?)<',genres,re.S|re.M|re.I|re.DOTALL)
+        except:genre='nada'
+        _H = ''
+        if genre!='nada':
+            for w in genre:
+                if w=='' or w=='  ':continue
+                w = w.replace('\n','').replace('\t','').replace('\r','').replace('\n\n','')
+                _H += w.decode('utf-8')+' '
+            print _H
+            MyDict_['genre']=_H
+    return MyDict_
+#######################################################################Import watch Link
+def get_D1(url):
+    MyListFilms = []
+    HDR = {'Host': 'w.cimalek.to',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+           'Accept': 'application/json, text/javascript, */*; q=0.01',
+           'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+           'Accept-Encoding': 'gzip, deflate',
+           'X-Requested-With': 'XMLHttpRequest',
+           'Connection': 'keep-alive',
+           'Referer': url}
+    _Hd = {'Host': 'zorona.cam',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+           'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+           'Accept-Encoding': 'gzip, deflate',
+           'Alt-Used': 'zorona.cam',
+           'Connection': 'keep-alive',
+           'Referer': 'https://w.cimalek.to/'}
+    rgx = '''<div id='player-option-.+?' class='btn lalaplay_player_option' data-type='(.+?)' data-post='(.+?)' data-nume='(.+?)'>'''
+    try:data = get_Data(url,get='get',typ='content')
+    except:data=''
+    if data:
+        try:Donnees = re.findall(rgx,data)
+        except: Donnees=''
+        if Donnees:
+            i = 1
+            for a,b,c in Donnees:#('Cloud_V1', '569243', '1')
+                link = "https://w.cimalek.to/wp-json/lalaplayer/v2/?p=%s&t=%s&n=%s" % (b,a,c)
+                try:_dat = get_Data(link,get='get',typ='json()',headers=HDR)
+                except:_dat=''
+                if _dat:
+                    _A = _dat['embed_url']
+                    try:NwDat = get_Data(_A,get='get',typ='content',headers=_Hd)
+                    except:NwDat=''
+                    if NwDat:
+                        try:
+                            _rgx  = '''sources":\{"file":"(.+?)","label":"(.+?)"'''
+                            _file = re.findall(_rgx,NwDat)[0]
+                            _file = _file.replace('\\','')
+                        except:
+                            try:
+                                _rgx = '''{"file":"(.+?)","label":"(.+?)","type"'''
+                                _file = re.findall(_rgx,NwDat)
+                            except:_file = 'nada'
+                        try:
+                            _rgx1 ='''"sub_id":.+?,"language":"(.+?)",.+?,"file":"(.+?)"'''
+                            sub_id = re.findall(_rgx1,NwDat)
+                        except:sub_id='nada'
+                        if _file!='nada':
+                            for lnko,label in _file:
+                                lnko = lnko.replace('\\','')
+                                MyListFilms.append((label,lnko))
+                        if sub_id!='nada':
+                            for lang,lnk in sub_id:
+                                lnk = lnk.replace('\\','')
+                                MyListFilms.append((lang,lnk))
+                    else:print 'nada_NwDat'
+    if len(MyListFilms)!=0:return True,MyListFilms
+    else:return False,[]
