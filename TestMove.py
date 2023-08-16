abb124c0714998a1dfc7b580eb2a99cf7c38979c
AbouYacine [aime_jeux] <aime_jeux@yahoo.fr>
2023-08-16 16:24:32 +0100

    Update TestMove.py

--- TestMove.py
+++ TestMove.py
@@ -5,23 +5,26 @@ from sys import version_info
 from Components.ActionMap import ActionMap
 ##################################################################
 from enigma import getDesktop
+from Components.Label import Label
 ################################################ yasser
 black,white,gray='\c00000000','\c00??????','\c00808080'
 blue,green,red,yellow,cyan,magenta,ivory='\c000000??','\c0000??00','\c00??0000','\c00????00','\c0000????','\c00??00??','\c0???????'
 ################################################ yasser
 PY3 = version_info[0] == 3
 ##################################################################My Imort
-from Plugins.Extensions.ShowMovies.Cimalek.OutilsCimalek.MyImportCimalek import ShowMovies_New,get_D1,get_Info_Film,get_Taille
-from Plugins.Extensions.ShowMovies.Cimalek.OutilsCimalek.AllImport import *
-from Plugins.Extensions.ShowMovies.Cimalek.Home.Watchability import HomeShowMoviesSelect
+from Plugins.Extensions.ShowMovies.CineMa.OutilsCineMa.MyImportCima import ShowMovies_New,get_D1,get_Info_Film,get_Taille
+from Plugins.Extensions.ShowMovies.CineMa.OutilsCineMa.AllImport import *
+from Plugins.Extensions.ShowMovies.CineMa.Home.Watchability import HomeShowMoviesSelect
+from Plugins.Extensions.ShowMovies.CineMa.Home.Seasons import HomeShowMoviesSeasons
 #########################################
 from enigma import eServiceReference
 #########################################
 #########################################
 dwidth = getDesktop(0).size().width()
 #########################################
+URLOTST = "https://imdb-video.media-imdb.com/vi1209648153/hls-preview-7fb41ae7-af9b-4224-a0f7-7bcc2988b433.m3u8?Expires=1691785913\u0026Signature=SsLUMYNVGies01G6k29474VAeHAudkfbC8c4DdbRoBfoI5StM~wLAlIFEnaSztnH~Nq5y0U5bHhypWs9nq6SYTtk9Bm2DBR3E9Caw3RGzNSeMjYa7IJ2mnaZb1fIv3oygGq~n-vbWRhE34SIKLR2kt7ogepE6DNEKOhpCsyIp2n2DnCG08NIyqrJUsVfwUbc~zZLlxRf-jdK0g49b7RboxIousWXxm3XfvMdGZRVScrxsozkTslBHyci12ex1esveJ0cXNcgyx-JtlIOAns0xgRUIqsZM~78wSZvakFu~c~dLrXUGkrlLNJcW4nBQa~xs93nCEUly3R5CDz3HaUIwA__\u0026Key-Pair-Id=APKAIFLZBVQZ24NQH3KA".replace('\u0026','&')
 class MenuShowMovies(Screen,ShowMovies_New):
-	def __init__(self, session, *args):
+	def __init__(self, session,Url=None):
 		ShowMovies_New.__init__(self)
 		if dwidth == 1280:
 		    with open(PLUGIN_PATH_SKIN + '/MainTestAnimFHD.xml', 'r') as f:
@@ -39,13 +42,24 @@ class MenuShowMovies(Screen,ShowMovies_New):
 			"down": self.keyDown,
 			"up": self.keyUp,
 			'ok': self.ok,
-			#'green': self.affich_DEbut,
+			'yellow': self.TestImdb,
 			'green': self.Import_My_Watch_Url,
 			'blue': self.Import_second_Page,
-			'yellow': self.Clear_Folder_Img,
+			#'yellow': self.Clear_Folder_Img,
 		}, -1)
+		if Url is not None:self.Url = Url
+		else:self.Url = None
+		self.onLayoutFinish.append(self.AffichTitles)
 		self.affich_DEbut()
 	##############################
+	def TestImdb(self):
+		from enigma import eServiceReference
+		from Screens.InfoBar import InfoBar, MoviePlayer
+		name = 'badi3elzeman'
+		stream_url = URLOTST
+		self.reference = eServiceReference(5002, 0, str(stream_url))
+		self.reference.setName(name)
+		self.session.open(MoviePlayer,self.reference)
 	def affich_DEbut(self):
 		self.onLayoutFinish.append(self.layoutFinish)
 		self.getposi_image()
@@ -72,22 +86,16 @@ class MenuShowMovies(Screen,ShowMovies_New):
 		self.Regroupement()
 	##############################
 	def left(self):
-		self['menu'].pageUp()
 		idx = self['menu'].getSelectionIndex()
+		if idx == 0:self['menu'].moveToIndex(len(self.menu)-1)
+		else:self['menu'].pageUp()
 		self.Regroupement()
 	##############################
 	def right(self):
-		self['menu'].pageDown()
 		idx = self['menu'].getSelectionIndex()
+		if idx == len(self.menu)-1:self['menu'].moveToIndex(0)
+		else:self['menu'].pageDown()
 		self.Regroupement()
-	##############################
-	# def Moveframe(self):
-		# self.yx = self.y11
-		# self.dyx = (self.y22 - self.y11) // 40
-		# if self.newupdateLabel in self.AnimTimer.callback:
-		    # self.AnimTimer.callback.remove(self.newupdateLabel)
-		    # self.AnimTimer.callback.append(self.showDescAnim)
-		# self.AnimTimer.start(100//10, True)
 	##############################
 	def Import_My_Watch_Url(self):
 		self.Import_My_Infos()
@@ -104,18 +112,6 @@ class MenuShowMovies(Screen,ShowMovies_New):
 		self.setText_Films()
 		self.TestRating()
 		self.Moveframe()
-	#############################
-	# def Import_My_Infos(self):
-		# if self.Msg_[0]:
-		    # index = self['menu'].getSelectionIndex()
-		    # _H = self.NewListJS.items()[index][1][1]
-		    # self._Info = get_Info_Film(_H)
-		    # self.watc = ''
-		    # i= 4
-		    # List_Secour = ['الاسم الاصلي'.encode('utf-8'),'البلد المنشئ'.encode('utf-8'),'المدة'.encode('utf-8'),'تاريخ العرض'.encode('utf-8'),'اللغة'.encode('utf-8')]
-		    # if len(self._Info)!=0:
-		        # _Title = self['menu'].getCurrent()[0]
-		        # self.session.open(HomeShowMoviesSelect,self._Info,_Title)
 	##############################
 	def ok(self):
 		if self.Msg_[0]:
@@ -129,7 +125,7 @@ class MenuShowMovies(Screen,ShowMovies_New):
 		self.close(True)
 	##############################
 	def Clear_Folder_Img(self):
-		Milef = '/media/hdd/Cimalek/Images/'
+		Milef = '/media/hdd/CineMa/Images/'
 		for root, dirs, files in os.walk(Milef):
 		    for f in files:
 		        os.unlink(os.path.join(root, f))
